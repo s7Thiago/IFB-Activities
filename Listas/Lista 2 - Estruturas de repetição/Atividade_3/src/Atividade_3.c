1279ed353ee119df29c79bef790955ebd0af52b2
s7Thiago <thyagosousasilva@gmail.com>
2018-06-22 20:49:33 -0300

    Adicionando arquivo pasta para a atividade 3 da lista

---
+++ Listas/Lista 2 - Estruturas de repetição/Atividade_3/src/Atividade_3.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : Atividade_3.c
+ Author      : Thiago Sousa
+ Version     : 1.0
+ Copyright   : all rights reserved
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	printf("Executando o meu primeiro programa em C feito no Eclipse!!!");
+
+return EXIT_SUCCESS;
+}
