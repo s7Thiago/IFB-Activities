f2545f411694bcd045ab1986cef9a28aedcaf7ee
s7thiago <thyagosousasilva@gmail.com>
2018-06-22 22:04:11 -0300

    Adicionando resposta para a atividade 6

---
+++ Listas/Lista 2 - Estruturas de repetição/Atividade_6_2/src/Main.java
@@ -0,0 +1,14 @@
+/*
+Porque para que um numero seja considerado primo,
+
+basta que ele tenha mais de um divisor. o que pode
+
+ser verificado até o meio do caminho, pois se até aí
+
+não houver mais de 2 números que satisfaçam o teste
+
+(n % i == 0), significa que o número so é divisível por
+
+1, e por ele mesmo.
+
+*/
