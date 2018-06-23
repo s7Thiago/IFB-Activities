8397d6bbde488168666e2ff57492c1393e5236f0
s7thiago <thyagosousasilva@gmail.com>
2018-06-22 21:09:07 -0300

    adicionando versão Java da atividade 3

---
+++ Listas/Lista 2 - Estruturas de repetição/Atividade_3_2/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;																																
+
+public class Main {
+	
+	
+	public static void main(String[] args) {
+		
+		//declarando variáveis auxiliares
+		int n, result = 0;
+		
+		//exibindo mensagem e armazenando a 
+		//entrada do teclado em n
+		System.out.print("Digite n: ");
+		n = new Scanner(System.in).nextInt();
+		
+		//somando todos os números de 1 a n
+		//e armazenando em result
+		for(int i = 1; i <= n; i++) {
+			
+			result = result += i;
+		}
+		
+		//imprimindo o valor final de result
+		System.out.println("\nResultado: " + result);
+	}
+}
