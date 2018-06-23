262a0b617758b3b7be08dcfe338d4ae93fdc574c
s7thiago <thyagosousasilva@gmail.com>
2018-06-22 22:09:27 -0300

    melhorando resposta

--- Listas/Lista 2 - Estruturas de repetição/Atividade_6_2/src/Main.java
+++ Listas/Lista 2 - Estruturas de repetição/Atividade_6_2/src/Main.java
@@ -7,7 +7,7 @@ ser verificado até o meio do caminho, pois se até aí
 
 não houver mais de 2 números que satisfaçam o teste
 
-(n % i == 0), significa que o número so é divisível por
+(n % i == 0), significa que tal número so é divisível por
 
 1, e por ele mesmo.
 
