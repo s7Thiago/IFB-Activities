4c4127a023c4df4587714049894624c6f508edf3
s7thiago <thyagosousasilva@gmail.com>
2018-07-03 16:19:53 -0300

    Pequenas modificações no código e melhoramento dos resultados

--- Listas/Lista 2 - Estruturas de repetição/Atividade_9_2/src/Main.java
+++ Listas/Lista 2 - Estruturas de repetição/Atividade_9_2/src/Main.java
@@ -1,8 +1,37 @@
-import java.util.*;
+import java.util.*;																																																																																																				
 
 public class Main {
 		
-	public static void main(String[] args) {
+		//método que verifica se o número do parâmetro é primo
+		public int ehPrimo(int num) {
+				int cont = 0;
+				
+				for(int i = 1; i <= num; i++) {
+						
+						//verificando se num é divisível por cada i
+						if(!(num % i == 0)) {
+								cont++;
+						}
+				}
+				
+				//retorna quantos divisores foram encontrados
+				return cont;
+		}
 		
+	public static void main(String[] args) {
+			Main m = new Main();
+			
+			int n, div1, div2;
+			
+			System.out.print("Digite n: ");
+			n = new Scanner(System.in).nextInt();
+			
+			//econtra o menor número primo que é maior do que n
+			for(int i = n; (m.ehPrimo(i) != 2); i++) {
+					
+					if(m.ehPrimo(i) < 3) {
+					System.out.println(i);
+					}
+			}
 	}
 }
