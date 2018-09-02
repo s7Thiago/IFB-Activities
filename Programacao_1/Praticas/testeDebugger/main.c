e91753f1263da4ea78cce768588d8bf0d36295e5
s7thiago <thyagosousasilva@gmail.com>
2018-09-02 12:43:21 -0300

    Adicionando exemplo de algoritmo que limpa o console

--- Programacao_1/Praticas/testeDebugger/main.c
+++ Programacao_1/Praticas/testeDebugger/main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-
+int main()
+{
     char nome[] = "";
-
     printf("\n\nDigite um nome: ");
     gets(nome);
 
@@ -16,9 +15,9 @@ int main() {
     printf("\n\nDigite um nome: ");
     scanf("%s", &nome2);
 
-    printf("\n\nOla, %s", nome2);
+    printf("\n\nOla, %s\n", nome2);
 
-    printf("Os nomes recebidos sao %s e %s.", nome, nome2);
+    printf("\n\nOs nomes recebidos sao %s e %s\n\n.", nome, nome2);
 
     return 0;
 }
