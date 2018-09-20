6aa9d1e4296eae4be964042c9b262f7015b02d80
s7thiago <thyagosousasilva@gmail.com>
2018-09-20 00:02:25 -0300

    Adicionando o código da questão

--- Programacao_1/Lista_funcoes/Atividade2/main.c
+++ Programacao_1/Lista_funcoes/Atividade2/main.c
@@ -7,6 +7,7 @@ float calcMedia(float nota1, float nota2, float nota3, char operacao) {
 
     switch(operacao) {
     case 'A':
+    case 'a':
         //Calcula a media normal
         media = ((nota1 + nota2 + nota3)/3);
 
@@ -14,21 +15,52 @@ float calcMedia(float nota1, float nota2, float nota3, char operacao) {
         break;
 
     case 'P':
+    case 'p':
         //Calcula a media ponderada
+        mediaPonderada = (((5 * nota1) + (3 * nota2) + (2 * nota3)));
+        return mediaPonderada;
         break;
 
     case 'H':
+    case 'h':
         //Calcula a media harmonica
+        mediaHarmonica = (3/(1/nota1) + (1/nota2) + (1/nota3));
+        return mediaHarmonica;
+        break;
+
+    case 'X': case 'x':
+        printf("Finalizando execucao");
         break;
 
     default:
         printf("Opcao invalida");
         break;
     }
+
+    return 0;
 }
 
-int main()
-{
-    printf("Hello world!\n");
+int main() {
+    float a, b, c, resultado;
+    char operacao;
+
+    do {
+    printf("\nDigite a operacao desejada 'A', 'P', ou 'H': ");
+    scanf("%c", &operacao);
+
+    printf("Digite a primeira nota: ");
+    scanf("%f", &a);
+
+    printf("Digite a segunda nota: ");
+    scanf("%f", &b);
+
+    printf("Digite a terceira nota: ");
+    scanf("%f", &c);
+
+    resultado = calcMedia(a, b, c, operacao);
+
+    printf("\n\nResultado: %.2f\n\n", resultado);
+    }while(operacao != 'x');
+
     return 0;
 }
