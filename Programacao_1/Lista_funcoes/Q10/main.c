f5f5d6a2166276815ffb70bd94823e640224779c
s7thiago <thyagosousasilva@gmail.com>
2018-09-20 23:53:06 -0300

    Codificando a questão 10

--- Programacao_1/Lista_funcoes/Q10/main.c
+++ Programacao_1/Lista_funcoes/Q10/main.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+#define TRUE 1
+#define FALSE 0
+
+int verificaPar(int valor) {
+    //Se retornar 1, o numero e par
+    if(valor % 2 == 0) {
+    return TRUE;
+
+    //Se retornar 0, o numero e impar.
+    }else {
+    return FALSE;
+    }
+}
+
+int main() {
+
+    int valor, teste;
+
+    printf("Digite algum valor inteiro: ");
+    scanf("%i", &valor);
+
+    teste = verificaPar(valor);
+
+    if(teste == 0) {
+            printf("\n\nValor impar\n\n");
+    } else {
+            printf("\n\nValor par\n\n");
+
+    }
     return 0;
 }
