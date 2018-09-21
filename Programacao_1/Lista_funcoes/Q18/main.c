1a9e1573a9c8e67e284292714c2b1220f06ea4cd
s7thiago <thyagosousasilva@gmail.com>
2018-09-21 01:17:56 -0300

    Adicionando mais atividades

--- Programacao_1/Lista_funcoes/Q18/main.c
+++ Programacao_1/Lista_funcoes/Q18/main.c
@@ -1,8 +1,49 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+//Retorna o fatorial de um número
+int fact(int num) {
+int factResult = 0;
+
+for(factResult = 1; num > 1; num = num - 1) {
+
+        factResult *= num;
+}
+
+return factResult;
+
+}
+
+void main() {
+
+    int num = 0;
+    char option = '0';
+
+//Execução do programa
+    do{
+
+    printf("\nDeseja contunuar? [s, n]: ");
+    scanf("%c", &option);
+
+    switch(option) {
+
+    case 's':
+        printf("Digite um número: ");
+        scanf("%i", &num);
+
+        printf("\n\nResultado: %i\n\n", fact(num));
+        break;
+
+    case 'n':
+
+    printf("\n\nFinalizando...\n\n");
+
+        break;
+
+    default:
+
+        printf("Opção inválida");
+    }
+}
+while(option != 'n');
+
 }
