9a87977376f6c44cf533656d451d0810a233b530
s7thiago <thyagosousasilva@gmail.com>
2018-10-19 14:16:08 -0300

    Corrigindo o código

--- Programacao_1/Structs-Example2/main.c
+++ Programacao_1/Structs-Example2/main.c
@@ -1,64 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define QTDEALUNOS 3;
+#define QTDEALUNOS 3
+
+//Definindo um tipo baseado na estrutura Aluno:
+    typedef struct Aluno tipoAluno;
 
 //Estrutura para se 'fabricar' um aluno
-struct Aluno
-{
+struct Aluno {
     int matricula;
     float n1, n2;
     float media;
 };
 
-float calcMedia(float *n1, float *n2)
-{
+float calcMedia(float *n1, float *n2) {
     return ((*n1 + *n2) / 2);
 }
 
-void lerDados(typedef struct Aluno alunos[], int quantidade)
-{
+void lerDados(tipoAluno *alunos) {
     int i;
-    for(i = 0; i < quantidade; i++)
-    {
+    for(i = 0; i < QTDEALUNOS; i++) {
         printf("Digite a matricula do aluno %i: ", (i + 1));
-        scanf("%i", &alunos[i].matricula);
+        scanf("%d", &alunos[i].matricula);
 
         printf("Digite a nota 1 do aluno %i: ", (i + 1));
         scanf("%f", &alunos[i].n1);
 
         printf("Digite a nota 2 do aluno %i: ", (i + 1));
         scanf("%f", &alunos[i].n2);
+
+        printf("\n");
     }
 }
 
-void imprimirDados(Aluno alunos[], int quantidade) {
+void imprimirDados(tipoAluno *alunos) {
     int i;
-    for(i = 0; i < quantidade; i++) {
-            printf("------Aluno %i------\n  -Matricula: %i\n  -Nota 1: %2f\n  -Nota 2: %.2f\n  -Media: %.2f", (i + 1), alunos[i].matricula, alunos[i].n1, alunos[i].n2, calcMedia(&alunos[i].n1, &alunos[i].n2));
+    for(i = 0; i < QTDEALUNOS; i++) {
+            printf("------Aluno %i------\n  -Matricula: %i\n  -Nota 1: %.2f\n  -Nota 2: %.2f\n  -Media: %.2f", (i + 1), alunos[i].matricula, alunos[i].n1, alunos[i].n2, calcMedia(&alunos[i].n1, &alunos[i].n2));
         printf("\n---------------------------\n\n");
     }
 }
 
 //Executa o programa
-void executar(Aluno alunos[], int quantidade) {
+void executar(tipoAluno *alunos[]) {
 
-    lerDados(alunos, quantidade);
-    imprimirDados(alunos, quantidade);
+    lerDados(alunos);
+    imprimirDados(alunos);
 
 }
 
-int main()
-{
-    //Definindo um tipo baseado na estrutura Aluno:
-    typedef struct Aluno tipoAluno;
+int main() {
+    //Criando os alunos
+    tipoAluno a1, a2, a3;
 
     //Criando as variaveis de tipoAluno:
-    tipoAluno colecaoAlunos[QTDEALUNOS] = {tipoAluno a1, tipoAluno a2, tipoAluno a3};
+    tipoAluno colecaoAlunos[QTDEALUNOS] = {a1, a2, a3};
+
 
     //Executando o programa
-    executar(colecaoAlunos, QTDEALUNOS);
+    executar(colecaoAlunos);
 
-    printf("Hello world!\n");
     return 0;
 }
