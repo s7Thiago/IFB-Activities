d5a5885c8f99d8e8591bc72ea9572dceae920a9c
Thiago Silva <37230912+s7Thiago@users.noreply.github.com>
2020-08-21 23:30:44 -0300

    implementando a lógica da grid 2D

--- 2020/IA/atividade_busca_cega/sketch.js
+++ 2020/IA/atividade_busca_cega/sketch.js
@@ -1,10 +1,34 @@
+var cols = 5;
+var rows = 5;
+var grid = new Array(cols);
+
+function Spot() {
+  this.f = 0;
+  this.g = 0;
+  this.h = 0;
+}
+
 function setup() {
   // put setup code here
   createCanvas(400, 400);
+  console.log("A*");
+
+  // filling grid (Making a 2D array)
+  for (var i = 0; i < cols; i++) {
+    grid[i] = new Array(rows);
+  }
+
+  //
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j] = new Spot();
+    }
+  }
+
+  console.log(grid);
 }
 
 function draw() {
   // put drawing code here
   background(0);
-  circle(200, 200, 200);
 }
