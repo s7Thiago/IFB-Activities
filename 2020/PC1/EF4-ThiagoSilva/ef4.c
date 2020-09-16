d6bc94c804c03780710ae4c71c22106eb175b549
s7thiago <thyagosousasilva@gmail.com>
2020-09-15 22:28:02 -0300

    Implementando EF4

--- 2020/PC1/EF4-ThiagoSilva/ef4.c
+++ 2020/PC1/EF4-ThiagoSilva/ef4.c
@@ -4,6 +4,8 @@
 #define TAM_NAME 25
 #define MAX_SONGS 10
 
+struct album target;
+
 char menu[4][TAM_NAME * 2] = {
 	"1 - Criar o álbum musical",
 	"2 - Adicionar música ao álbum",
@@ -13,6 +15,7 @@ char menu[4][TAM_NAME * 2] = {
 
 //Imprime o menu de opções
 void showMenu() {
+	printf("\n");
 	for (int i = 0; i < 4; i++){
 		printf("%s\n", menu[i]);
 	}
@@ -34,35 +37,90 @@ void showMenu() {
 		char generoMusical[TAM_NAME];
 		int qtdeMusicas;
 		struct musica musicas[MAX_SONGS];
-	}album;
+	};
+	
+//Limpa uma string
+void cleanString(char *target) {
+	for (int i = 0; i < strlen(target); i++){
+		target[i] = '\0';
+	}
+}	
+
+// Reseta o album
+void cleanAlbum(){
+	//Atribuindo
+	cleanString(target.nomeAlbum);
+	cleanString(target.generoMusical);
+	target.qtdeMusicas = 0;
+}
+	
+// Cria o album
+void novoAlbum(){
+	//Album flow
+	char nomeAlbum[TAM_NAME];
+	char generoMusical[TAM_NAME];
+	int qtdeMusicas;
+	
+	//Nome
+	printf("Nome: ");
+	scanf("%s", nomeAlbum);
+	
+	//genero
+	printf("Gênero musical: ");
+	scanf("%s", generoMusical);
+	
+	//qtde
+	printf("Qtde de músicas: ");
+	scanf("%d", &qtdeMusicas);
+	
+	//Atribuindo
+	strcpy(target.nomeAlbum, nomeAlbum);
+	strcpy(target.generoMusical, generoMusical);
+	target.qtdeMusicas = qtdeMusicas;
+	
+	//
+	printf("\nINFO:\nnome: %s\ngenero: %s\nfaixas: %d\n", 
+	target.nomeAlbum, target.generoMusical, target.qtdeMusicas);
+}
+
+void adicionarMusica() {
+	
+}
 
 int main (void) {
 
 	int option;
 	
 	do{
-	//
-	showMenu();
+		//
+		showMenu();
 	
-	printf("Escolha > ");
-	scanf("%d", &option);
+		printf("Escolha > ");
+		scanf("%d", &option);
 	
 	switch(option) {
 	
-	case 1:
-	break;
+		case 1:
+			novoAlbum();
+		break;
 	
-	case 2:
-	break;
+		case 2:
+		break;
 	
-	case 3:
-	break;
+		case 3:
+		break;
 	
-	case 4:
-	break;
+		case 4:
+		break;
 	
-	default:
-		printf("Opção inválida");
+		case 5:
+			printf("\nINFO:\nnome: %s\ngenero: %s\nfaixas: %d\n", 
+			target.nomeAlbum, target.generoMusical, target.qtdeMusicas);
+		break;
+	
+		default:
+			printf("\n\nOpção Inválida!\n\n");
+			cleanAlbum();
 	}
 	
 	}while(option != 4);
