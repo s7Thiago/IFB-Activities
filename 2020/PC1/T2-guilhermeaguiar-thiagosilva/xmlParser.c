2a770c08dd87e4d215e77ec38e151a597403ef72
kadeguilherme <kadeguilherme@gmail.com>
2020-10-27 14:18:15 -0300

    Removendo funçãoes desnecessarias

--- 2020/PC1/T2-guilhermeaguiar-thiagosilva/xmlParser.c
+++ 2020/PC1/T2-guilhermeaguiar-thiagosilva/xmlParser.c
@@ -7,32 +7,6 @@ char *title = "title";
 char *description = "description";
 char *link = "link";
 
-void procurarTag(FILE *fp,char tag[20]){
-
-    int i = 0;
-    int ch;
-  
-
-    while( ((ch = fgetc(fp)) != EOF) && (ch != '>') && (i < 19)){
-        tag[i] = ch;
-        i++;       
-    }
-    tag[i] = '\0';
-}
-
-
-void pegarTag(FILE *fp, FILE *html){
-    int ch;
-
-    fseek(fp,9,SEEK_CUR);
-
-    while ( ((ch = fgetc(fp))!= EOF) && (ch !=']')){
-        fprintf(html,"%c",ch);
-    }
-fprintf(html,"\n");
-    
-}
-
 // ######### Novas
 void pupulateData(){
 	FILE* fp;
@@ -98,12 +72,9 @@ void getTag(char* searchPlace, char* tagName){
 //                 Copia o conteúdo seguinte para `tagContents` até encontrar 
 //                 a tag de fechamento
                 for(int k = j; k < strlen(searchPlace); k++) {
-                     
-					 
-					 
-					 printf("%c",searchPlace[k]);
-					 
-//                     Buscando tag de fechamento correspondente
+                    
+					 printf("%c",searchPlace[k]);			 
+//                   Buscando tag de fechamento correspondente
                         if(searchPlace[k + 1] == '<' && searchPlace[k + 2] == '/'){					
 							printf("\n");
 							break;
