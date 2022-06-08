1604d475ee48687e08a363c3ac0a37b43283f7c4
Thiago Silva <thyagosousasilva@gmail.com>
2022-06-08 09:46:06 -0300

    Adicionando exemplo da aula 8

---
+++ 2022/optativa1/aula_local_api/outras_aulas/web_api/lib/model/curso.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+import 'package:web_api/http/web_client.dart';
+
+class Curso {
+  int? id;
+  String nome;
+  int cargaHoraria;
+  String modalidade;
+  Curso(this.nome, this.cargaHoraria, this.modalidade, {this.id});
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "nome": nome,
+        "cargaHoraria": cargaHoraria,
+        "modalidade": modalidade,
+      };
+  Curso.fromJson(Map<String, dynamic> json)
+      : nome = json["nome"],
+        cargaHoraria = json["cargaHoraria"],
+        modalidade = json["modalidade"],
+        id = json["id"];
+  @override
+  String toString() {
+    return "Curso: $nome \n Carga Horária: $cargaHoraria \n modalidade: $modalidade";
+  }
+}
+
+Future<List<Curso>> findAll() async {
+  final Client client =
+      InterceptedClient.build(interceptors: [LoggingInterceptor()]);
+  final Response response =
+      await client.get(Uri.http('192.168.59.108:8080', 'api/v1/cursos'));
+  final List<dynamic> decodedJson = jsonDecode(utf8.decode(response.bodyBytes));
+  final List<Curso> cursos = [];
+  for (Map<String, dynamic> cursoJson in decodedJson) {
+    final Curso curso = Curso(
+      cursoJson['nome'],
+      cursoJson['cargaHoraria'],
+      cursoJson['modalidade'],
+      id: cursoJson['id'],
+    );
+    cursos.add(curso);
+  }
+  return cursos;
+}
