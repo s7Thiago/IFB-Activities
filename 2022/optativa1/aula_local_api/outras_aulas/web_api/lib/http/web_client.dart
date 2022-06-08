cedd4df746a4b51361544b995b2f735793b13ffa
Thiago Silva <thyagosousasilva@gmail.com>
2022-06-08 11:37:32 -0300

    Removendo projeto desnecessário

--- 2022/optativa1/aula_local_api/outras_aulas/web_api/lib/http/web_client.dart
+++
@@ -1,8 +0,0 @@
-import 'package:http_interceptor/http_interceptor.dart';
-import 'package:http/http.dart';
-import 'interceptor/logging_interceptor.dart';
-
-final Client client =
-    InterceptedClient.build(interceptors: [LoggingInterceptor()]);
-const String baseUrl = '192.168.59.108:8080';
-const String api = 'api/v1/cursos';
